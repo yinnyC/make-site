e6a0248341b9d323a639d9553491f056f81f3d62
Yin <yinnnn17@gmail.com>
2021-09-24 16:41:18 -0700

    Finished v1.1 requirements

--- makesite.go
+++ makesite.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
@@ -15,16 +16,21 @@ type Text struct {
 
 func main() {
 	var filename string
+	var directory string
 
-	flag.StringVar(&filename, "f", "", "name of file to write to html")
 	flag.StringVar(&filename, "file", "", "name of file to write to html")
-
+	flag.StringVar(&directory, "dir", "", "name of directory to get all txt files to write to html")
 	flag.Parse()
 
-	fileContent := readFile(filename)
-	fileToWrite := strings.SplitN(filename, ".", 2)[0]
+	if directory != "" {
+		printAllTxtFiles(directory)
+	}
+	if filename != "" {
+		fileContent := readFile(filename)
+		fileToWrite := stripExt(filename)
 
-	writeTemplate("template.tmpl", fileToWrite, string(fileContent))
+		writeToHTML("template.tmpl", fileToWrite, string(fileContent))
+	}
 }
 
 func readFile(file string) []byte {
@@ -34,17 +40,39 @@ func readFile(file string) []byte {
 	}
 	return content
 }
-func writeTemplate(tmpl, filename, fileContent string) {
+func writeToHTML(tmpl, filename, fileContent string) {
 	text := Text{fileContent}
-
 	htmlFile, osErr := os.Create(filename + ".html")
 	if osErr != nil {
 		log.Fatal(osErr)
 	}
-
 	t := template.Must(template.ParseFiles(tmpl))
 	execErr := t.Execute(htmlFile, text)
 	if execErr != nil {
 		log.Fatal(execErr)
 	}
+}
+func printAllTxtFiles(directory string) {
+	files, err := os.ReadDir(directory)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, file := range files {
+		if isTxt(file.Name()) {
+			fmt.Println(file.Name())
+			filename := stripExt(file.Name())
+			fileContent := readFile(file.Name())
+			writeToHTML("template.tmpl", filename, string(fileContent))
+		}
+	}
+}
+
+func isTxt(filename string) bool {
+	fileExt := filename[len(filename)-4:]
+	return fileExt == ".txt"
+}
+
+func stripExt(filename string) string {
+	return strings.SplitN(filename, ".", 2)[0]
 }
