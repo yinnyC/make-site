4bfef3e0397f5163f13f47acf4bd5ce2f9ea29e7
Yin <yinnnn17@gmail.com>
2021-09-24 13:50:42 -0700

    Manually test the generated HTML page by running `./makesite`. Double-click the `first-post.html` file that appears in your directory after running the command to open the generated page in your browser.

--- makesite.go
+++ makesite.go
@@ -3,13 +3,39 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"log"
+	"os"
+	"text/template"
 )
 
+type Text struct {
+	Content string
+}
+
 func main() {
-	fmt.Println("Hello, world!")
-	fileContents, err := ioutil.ReadFile("first-post.txt")
+	fileContent := readFile("first-post.txt")
+	fmt.Print(string(fileContent))
+	writeTemplate("template.tmpl", "first-post", string(fileContent))
+}
+
+func readFile(file string) []byte {
+	content, err := ioutil.ReadFile(file)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
-	fmt.Print(string(fileContents))
+	return content
+}
+func writeTemplate(tmpl, filename, fileContent string) {
+	text := Text{fileContent}
+
+	htmlFile, osErr := os.Create(filename + ".html")
+	if osErr != nil {
+		log.Fatal(osErr)
+	}
+
+	t := template.Must(template.ParseFiles(tmpl))
+	execErr := t.Execute(htmlFile, text)
+	if execErr != nil {
+		log.Fatal(execErr)
+	}
 }
